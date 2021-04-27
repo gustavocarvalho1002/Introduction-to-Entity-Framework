b4e8f64b7d937ba197384e64107baf764e0cc630
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2021-04-27 11:51:12 -0300

    Using FluentAPI to map objects

--- ConsoleApp/ConsoleApp/Data/ApplicationContext.cs
+++ ConsoleApp/ConsoleApp/Data/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿
+using ConsoleApp.Domain;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
 
@@ -9,6 +10,65 @@ namespace ConsoleApp.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-3M4K2KV\\SQLEXPRESS;Initial Catalog=ConsoleAppDataBase;Trusted_Connection=True;")
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(p =>
+            {
+                p.ToTable("Clientes");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+                p.Property(p => p.Telefone).HasColumnType("CHAR(11)");
+                p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
+                p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
+                p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
+
+                p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone"); //Index for telefone search
+            });
+
+            modelBuilder.Entity<Produto>(p =>
+            {
+                p.ToTable("Pedidos");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(14)").IsRequired();
+                p.Property(p => p.Descricao).HasColumnType("VARCHAR(60)");
+                p.Property(p => p.Valor).IsRequired();
+                p.Property(p => p.TipoProduto).HasConversion<string>();
+            });
+
+            modelBuilder.Entity<Produto>(p =>
+            {
+                p.ToTable("Pedidos");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(14)").IsRequired();
+                p.Property(p => p.Descricao).HasColumnType("VARCHAR(60)");
+                p.Property(p => p.Valor).IsRequired();
+                p.Property(p => p.TipoProduto).HasConversion<string>();
+            });
+
+            modelBuilder.Entity<Pedido>(p =>
+            {
+                p.ToTable("Pedido");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
+                p.Property(p => p.StatusPedido).HasConversion<string>();
+                p.Property(p => p.TipoFrete).HasConversion<string>();
+                p.Property(p => p.Observacao).HasColumnType("VARCHAR(512)");
+
+                p.HasMany(p => p.Itens)
+                .WithOne(p => p.Pedido)
+                .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<PedidoItem>(p =>
+            {
+                p.ToTable("PedidoItens");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.Quantidade).HasDefaultValue(1).IsRequired();
+                p.Property(p => p.Valor).IsRequired();
+                p.Property(p => p.Desconto).IsRequired();
+            });
         }
     }
 }
