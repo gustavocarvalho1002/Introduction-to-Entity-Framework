f162bdb03281a2806318bcd4e32df5cb892e9e5e
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2021-04-27 11:29:57 -0300

    Creating aplication DbContext

---
+++ ConsoleApp/ConsoleApp/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace ConsoleApp.Data
+{
+    class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-3M4K2KV\\SQLEXPRESS;Initial Catalog=ConsoleAppDataBase;Trusted_Connection=True;")
+        }
+    }
+}
